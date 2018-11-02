eaa40e29c19b61706463810fd4fcc8ba59f5905a
lavaglijder <samvanderlugt@outlook.com>
2018-11-02 19:11:54 +0100

    Reset of the project! Preparing on version 2.0!
    + Added main class with basic code!

--- src/main/java/com/lavaglijder/warp/Warp.java
+++ src/main/java/com/lavaglijder/warp/Warp.java
@@ -1,16 +1,38 @@
 package com.lavaglijder.warp;
 
+import lavaglijder.com.github.lavautils.lavaapi.LavaAPI;
+import lavaglijder.com.github.lavautils.lavaapi.fileapi.File;
+import lavaglijder.com.github.lavautils.lavaapi.fileapi.FileAPI;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Warp extends JavaPlugin {
 
+    private static LavaAPI lavaAPI;
+    private static FileAPI fileAPI;
+
     @Override
     public void onEnable() {
+        lavaAPI = new LavaAPI("Warp");
+        fileAPI = new FileAPI(this);
 
+        fileAPI.addFile("config");
+        fileAPI.addFile("warps");
+        File configFile = fileAPI.getFile("config");
+
+        getLogger().info("Warp has been enabled");
     }
 
     @Override
     public void onDisable() {
 
+        getLogger().info("Warp has been enabled");
+    }
+
+    public static LavaAPI getLavaAPI() {
+        return lavaAPI;
+    }
+
+    public static FileAPI getFileAPI() {
+        return fileAPI;
     }
 }
