bdf9eb6bb36736241a4b1875b3efa7dd7cc8e273
lavaglijder <samvanderlugt@outlook.com>
2018-11-02 18:43:34 +0100

    Reset of the project! Preparing on version 2.0!

---
+++ src/main/java/com/lavaglijder/warp/Warp.java
@@ -0,0 +1,16 @@
+package com.lavaglijder.warp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Warp extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
